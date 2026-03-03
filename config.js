d116de6229bd071d2ae6eb7be9ade44e74c72ade
Resonance-coder <youthconnect.lin@gmail.com>
2026-03-03 22:07:01 +0700

    Update GAS URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__FORM_CONFIG__ = {
   // Paste your deployed Google Apps Script Web App /exec URL.
   // Example: "https://script.google.com/macros/s/XXXX/exec"
-  gasUrl: "https://script.google.com/macros/s/AKfycbz9aymX743V7bzOPNAGUdsUCZp6ai2wz_fCo6Baes9dekocOfRWdFORXvDyeGqBOnNe/exec"
+  gasUrl: "https://script.google.com/macros/s/AKfycbyrqFTPNwHQddpsEl--w7voYus_ge47_WdXfBNj-Gd-asMcPiCjSbKlSCGHTouMdchK/exec"
 };
