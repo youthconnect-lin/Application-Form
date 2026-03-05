4c4d5aff6deccf7a73d20030fda0e973d27e7d0b
Resonance-coder <youthconnect.lin@gmail.com>
2026-03-05 22:04:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.__FORM_CONFIG__ = {
   // Web App URL from deployed Google Apps Script (/exec).
-  gasUrl: "https://script.google.com/macros/s/AKfycbyrqFTPNwHQddpsEl--w7voYus_ge47_WdXfBNj-Gd-asMcPiCjSbKlSCGHTouMdchK/exec",
+  gasUrl: "https://script.google.com/macros/s/AKfycbz9aymX743V7bzOPNAGUdsUCZp6ai2wz_fCo6Baes9dekocOfRWdFORXvDyeGqBOnNe/exec",
 
   // Target spreadsheet context.
-  spreadsheetId: "1tobzP0pKivUR9ssgJSA9WrpzPxUxbfBe6YWe1dLH9H4",
-  spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1tobzP0pKivUR9ssgJSA9WrpzPxUxbfBe6YWe1dLH9H4/edit?gid=0#gid=0",
-  sheetGid: "0"
+  spreadsheetId: "1Rk_9ZEbu8kx-h_E0v7VnWgCoJafp1ZvaSUk8DgaVgH4",
+  spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1Rk_9ZEbu8kx-h_E0v7VnWgCoJafp1ZvaSUk8DgaVgH4/edit?gid=828377824#gid=828377824",
+  sheetGid: "828377824"
 };
